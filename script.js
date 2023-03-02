d86de3c2ebd3850b4d8caa69c92cafe0e89deceb
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-03-02 16:22:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Variaveis */
+/* var num1; 
+let num1;
+ */const PI = 3.1415;
+var num2 = 10;
+var num3 = 11;
+/* Operadores matemáticos */
+/* var num1 = 5; */ /* Varivael number */
+/* var num1 = "5" */; /* Varivael String */
+
+/* Variaveis em lista */
+var listnumber  = [];
+var lisnumber = <input type="text" ></input>
+document.write(listnumber);
+
+/* Formulas */
+
+var soma = num2 + num3;
+document.write('Resultado de num1 = ', soma)
